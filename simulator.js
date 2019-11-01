9913708238829a841b20c5b02458329790161e0b
JessieSunFX <sunjuan@19pay.com.cn>
2019-11-01 20:09:51 +0800

    no.17 1.17.10

--- simulator.js
+++ simulator.js
@@ -1,6 +1,25 @@
 const automater = require('miniprogram-automator');
 const path = require('path');
 
-const miniprogram = automater.launch({
+const miniprogram = automater.launch({//当前路径下启动一下模拟器
+    cliPath: 'path/to/cli', // 工具 cli 位置，如果你没有更改过默认安装位置，可以忽略此项 TODO
     projectPath: path.resolve(__dirname)
+})
+.then(miniprogram => {
+    miniprogram
+        .currentPage()
+        .then(page => {
+            return page
+                .waitFor(1000)
+                .then(() => page);
+        })
+        .then(page => page.$('multiplepic'))
+        .then(element => {
+            if (element && element.tagName === 'multiplepic') {
+                console.log('测试通过');
+            } else {
+                console.log('测试不通过');
+                console.log('element:::', element);
+            }
+        });
 });
